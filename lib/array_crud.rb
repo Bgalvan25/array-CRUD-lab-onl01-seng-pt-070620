d8adfb6633a5f5efca01bc195d0e185695d62d7f
Brittney Galvan <brittney.galvan25@yahoo.com>
2020-07-10 21:03:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ end
 def add_element_to_start_of_array(array, element)
  girl_names = ["Brittney" , "Amanda", "Gabriela" , "Lexi"] 
   girl_names.unshift("Natascha")
-  
+  puts girl_names.inspect 
 end
 
 def remove_element_from_end_of_array(array)
