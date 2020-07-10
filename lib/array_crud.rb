bd017e96dc4d626a220a8081699835c40db9a88a
Brittney Galvan <brittney.galvan25@yahoo.com>
2020-07-10 21:21:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,36 +13,36 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
- girl_names = ["Brittney" , "Amanda", "Gabriela" , "Lexi"] 
+ #girl_names = ["Brittney" , "Amanda", "Gabriela" , "Lexi"] 
   girl_names.unshift("Natascha")
   puts girl_names.inspect 
 end
 
 def remove_element_from_end_of_array(array)
-  girl_names = ["Brittney" , "Amanda", "Gabriela" , "Lexi"]
+  #girl_names = ["Brittney" , "Amanda", "Gabriela" , "Lexi"]
   "Lexi_name" = girl_names.pop
   puts girl_names.inspect
   puts "Lexi_name"
   end
 
 def remove_element_from_start_of_array(array)
-  girl_names = ["Brittney" , "Amanda", "Gabriela" , "Lexi"]
+  #girl_names = ["Brittney" , "Amanda", "Gabriela" , "Lexi"]
   "Brittney_name" = girl_names.shift 
   puts girl_names.inspect
   puts "Brittney_name"
 end
 
 def retrieve_element_from_index(array, index_number)
-girl_names = ["Brittney" , "Amanda", "Gabriela" , "Lexi"]
+#girl_names = ["Brittney" , "Amanda", "Gabriela" , "Lexi"]
 girl_names[2]
 end
 
 def retrieve_first_element_from_array(array)
-  girl_names = ["Brittney" , "Amanda", "Gabriela" , "Lexi"]
+  #girl_names = ["Brittney" , "Amanda", "Gabriela" , "Lexi"]
   girl_names[0]
 end
 
 def retrieve_last_element_from_array(array)
-  girl_names = ["Brittney" , "Amanda", "Gabriela" , "Lexi"]
+  #girl_names = ["Brittney" , "Amanda", "Gabriela" , "Lexi"]
   girl_names[-1]
 end
